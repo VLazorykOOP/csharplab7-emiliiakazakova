ef037a0d7255058f26fc8f57eec18db94b18e170
emiliiakazakova <kazakova.emiliia@chnu.edu.ua>
2024-06-17 01:25:16 +0300

    done

--- Lab7CSharp/Program.cs
+++ Lab7CSharp/Program.cs
@@ -8,15 +8,31 @@ namespace Lab7CSharp
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            Form1 form1 = new Form1();
+            form1.FormClosed += (sender, e) => OpenForm2();
+            Application.Run(form1);
+        }
+
+        static void OpenForm2()
+        {
+
+            Form2 form2 = new Form2();
+            form2.Load += (sender, e) => { /* Ваш код, який викликається при завантаженні Form2 */ };
+            form2.FormClosed += (sender, e) => OpenForm3();
+            form2.ShowDialog(); // Використовуйте ShowDialog() замість Application.Run() для Form2
+        }
+        static void OpenForm3()
+        {
+
+            Form3 form3 = new Form3();
+            form3.Load += (sender, e) => { /* Ваш код, який викликається при завантаженні Form2 */ };
+            form3.ShowDialog(); // Використовуйте ShowDialog() замість Application.Run() для Form2
         }
     }
 }
---
+++ Lab7CSharp/Shapes.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7CSharp
+{
+    using System.Drawing;
+
+    public abstract class Shape
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Color Color { get; set; }
+
+        public Shape(int x, int y, Color color)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+        }
+
+        public abstract void Draw(Graphics g);
+    }
+
+    public class Hexagon : Shape
+    {
+        public int Side { get; set; }
+
+        public Hexagon(int x, int y, int side, Color color)
+            : base(x, y, color)
+        {
+            Side = side;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            Brush brush = new SolidBrush(Color);
+            Point[] points = new Point[6];
+            for (int i = 0; i < 6; i++)
+            {
+                double angle = 2 * Math.PI / 6 * i;
+                int x = (int)(X + Side * Math.Cos(angle));
+                int y = (int)(Y + Side * Math.Sin(angle));
+                points[i] = new Point(x, y);
+            }
+            g.FillPolygon(brush, points);
+            brush.Dispose();
+        }
+    }
+
+    public class Pentagon : Shape
+    {
+        public int Side { get; set; }
+
+        public Pentagon(int x, int y, int side, Color color)
+           : base(x, y, color)
+        {
+            Side = side;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            Brush brush = new SolidBrush(Color);
+            Point[] points = new Point[5];
+            for (int i = 0; i < 5; i++)
+            {
+                double angle = 2 * Math.PI / 5 * i;
+                int x = (int)(X + Side * Math.Cos(angle));
+                int y = (int)(Y + Side * Math.Sin(angle));
+                points[i] = new Point(x, y);
+            }
+            g.FillPolygon(brush, points);
+            brush.Dispose();
+        }
+    }
+
+    public class Octagon : Shape
+    {
+        public int Side { get; set; }
+
+        public Octagon(int x, int y, int side, Color color)
+           : base(x, y, color)
+        {
+            Side = side;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            Brush brush = new SolidBrush(Color);
+            Point[] points = new Point[8];
+            for (int i = 0; i < 8; i++)
+            {
+                double angle = 2 * Math.PI / 8 * i;
+                int x = (int)(X + Side * Math.Cos(angle));
+                int y = (int)(Y + Side * Math.Sin(angle));
+                points[i] = new Point(x, y);
+            }
+            g.FillPolygon(brush, points);
+            brush.Dispose();
+        }
+    }
+}
